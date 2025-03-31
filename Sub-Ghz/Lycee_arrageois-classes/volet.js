c836a6e2ab555a2a0ee6daa966d902634118d83a
Lenigobrick <122463955+Lenigobrick@users.noreply.github.com>
2025-03-31 21:28:19 +0200

    XXXXX

--- Sub-Ghz/Lycee_arrageois-classes/volet.js
+++ Sub-Ghz/Lycee_arrageois-classes/volet.js
@@ -16,47 +16,47 @@ let nbVolet = 16;
 if (result === 0) {
     for (let i = 0; i < 2; i++) {
         print("pass: ", i+1, "/ 2")
-        print("0/" + nbVolet)
+        print("0/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/r3/1_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/r3/2_down_all.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/r3/3_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/r3/4_down_all.sub", 5);
-        print("1/" + nbVolet)
+        print("1/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/d5/1_down_all.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/d5/2_down.sub", 5);
-        print("2/" + nbVolet)
+        print("2/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/d6/1_down.sub", 5);
-        print("3/" + nbVolet)
+        print("3/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/d7/1_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/d7/2_down.sub", 5);
-        print("4/" + nbVolet)
+        print("4/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/g6/1_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/g6/2_down.sub", 5);
-        print("5/" + nbVolet)
+        print("5/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/g7/1_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/g7/2_down.sub", 5);
-        print("6/" + nbVolet)
+        print("6/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/g10/1_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/g10/2_down.sub", 5);
-        print("7/" + nbVolet)
+        print("7/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/g2/1_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/g2/2_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/g2/3_down.sub", 5);
-        print("8/" + nbVolet)
+        print("8/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/g5/1_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/g5/2_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/g5/3_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/g5/4_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/g5/5_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/g5/6_down.sub", 5);
-        print("9/" + nbVolet)
+        print("9/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/sp5/1_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/sp5/2_down.sub", 5);
-        print("10/" + nbVolet)
+        print("10/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/sp7/1_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/sp7/2_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/sp7/3_down.sub", 5);
-        print("11/" + nbVolet)
+        print("11/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/etude/1_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/etude/2_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/etude/3_down.sub", 5);
@@ -72,73 +72,73 @@ if (result === 0) {
         subghz.transmitFile("/ext/subghz/lycee/etude/13_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/etude/14_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/etude/15_down.sub", 5);
-        print("12/" + nbVolet)
+        print("12/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/unk1/1_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/unk1/2_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/unk1/3_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/unk1/4_down.sub", 5);
-        print("13/" + nbVolet)
+        print("13/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/unk2/1_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/unk2/2_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/unk2/3_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/unk2/4_down.sub", 5);
-        print("14/" + nbVolet)
+        print("14/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/unk3/1_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/unk3/2_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/unk3/3_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/unk3/4_down.sub", 5);
-        print("15/" + nbVolet)
+        print("15/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/unk4/1_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/unk4/2_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/unk4/3_down.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/unk4/4_down.sub", 5);
-        print("16/" + nbVolet)
+        print("16/", nbVolet)
     }
     notify.success();
 } else if (result === 1) {
     for (let i = 0; i < 2; i++) {
         print("pass: ", i+1, "/ 2")
-        print("0/" + nbVolet)
+        print("0/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/r3/1_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/r3/2_up_all.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/r3/3_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/r3/4_up_all.sub", 5);
-        print("1/" + nbVolet)
+        print("1/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/d5/1_up_all.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/d5/2_up.sub", 5);
-        print("2/" + nbVolet)
+        print("2/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/d6/1_up.sub", 5);
-        print("3/" + nbVolet)
+        print("3/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/d7/1_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/d7/2_up.sub", 5);
-        print("4/" + nbVolet)
+        print("4/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/g6/1_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/g6/2_up.sub", 5);
-        print("5/" + nbVolet)
+        print("5/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/g7/1_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/g7/2_up.sub", 5);
-        print("6/" + nbVolet)
+        print("6/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/g10/1_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/g10/2_up.sub", 5);
-        print("7/" + nbVolet)
+        print("7/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/g2/1_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/g2/2_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/g2/3_up.sub", 5);
-        print("8/" + nbVolet)
+        print("8/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/g5/1_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/g5/2_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/g5/3_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/g5/4_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/g5/5_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/g5/6_up.sub", 5);
-        print("9/" + nbVolet)
+        print("9/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/sp5/1_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/sp5/2_up.sub", 5);
-        print("10/" + nbVolet)
+        print("10/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/sp7/1_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/sp7/2_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/sp7/3_up.sub", 5);
-        print("11/" + nbVolet)
+        print("11/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/etude/1_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/etude/2_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/etude/3_up.sub", 5);
@@ -154,27 +154,27 @@ if (result === 0) {
         subghz.transmitFile("/ext/subghz/lycee/etude/13_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/etude/14_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/etude/15_up.sub", 5);
-        print("12/" + nbVolet)
+        print("12/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/unk1/1_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/unk1/2_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/unk1/3_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/unk1/4_up.sub", 5);
-        print("13/" + nbVolet)
+        print("13/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/unk2/1_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/unk2/2_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/unk2/3_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/unk2/4_up.sub", 5);
-        print("14/" + nbVolet)
+        print("14/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/unk3/1_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/unk3/2_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/unk3/3_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/unk3/4_up.sub", 5);
-        print("15/" + nbVolet)
+        print("15/", nbVolet)
         subghz.transmitFile("/ext/subghz/lycee/unk4/1_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/unk4/2_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/unk4/3_up.sub", 5);
         subghz.transmitFile("/ext/subghz/lycee/unk4/4_up.sub", 5);
-        print("16/" + nbVolet)
+        print("16/", nbVolet)
     }
     notify.success();
 } else if (result === 2){
